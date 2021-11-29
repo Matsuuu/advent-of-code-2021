bf5996b4747d1967415b11fa2742994888b5f4a9
Matsuuu <huhta.matias@gmail.com>
2021-11-29 19:59:54 +0200

    Add runner

--- in-case-i-want-to-do-java/aoc/src/main/java/matsu/App.java
+++ in-case-i-want-to-do-java/aoc/src/main/java/matsu/App.java
@@ -1,7 +1,7 @@
 package matsu;
 
 public class App {
-    public static void main( String[] args ) {
+    public static void main(String[] args) {
         System.out.println("It works?");
     }
 }
