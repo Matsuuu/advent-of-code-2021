6e8f0dfb10e8943986c7a9e38661d08bbf2eca34
Matsuuu <huhta.matias@gmail.com>
2021-11-29 20:11:31 +0200

    Add QoL

--- in-case-i-want-to-do-java/aoc/src/main/java/matsu/App.java
+++ in-case-i-want-to-do-java/aoc/src/main/java/matsu/App.java
@@ -2,6 +2,8 @@ package matsu;
 
 public class App {
     public static void main(String[] args) {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
         System.out.println("It works?");
     }
 }
